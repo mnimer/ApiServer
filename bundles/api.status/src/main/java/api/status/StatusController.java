08ac3618f154a641be0479e74b1f273ea2cd5e8f
Mike Nimer <mikenimer@yahoo.com>
2012-09-14 23:44:22 -0500

    commit before branching

--- bundles/api.status/src/main/java/api/status/StatusController.java
+++ bundles/api.status/src/main/java/api/status/StatusController.java
@@ -1,11 +1,16 @@
 package api.status;
 
+import org.osgi.framework.BundleContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.xml.ws.RequestWrapper;
 import java.util.Date;
+
 
 /**
  * User: mnimer
@@ -16,9 +21,13 @@ import java.util.Date;
 public class StatusController
 {
 
+
     @RequestMapping("/ping")
-    public ModelAndView systemCheck()
+    public ModelAndView systemCheck(HttpServletRequest request)
     {
+        BundleContext ctxt = (BundleContext) request.getSession().getServletContext().getAttribute("osgi-bundlecontext");
+        System.out.println(ctxt.toString());
+
         ModelAndView view = new ModelAndView("help");
         view.addObject("status", "ok");
         view.addObject("timestamp", new Date().toGMTString());
